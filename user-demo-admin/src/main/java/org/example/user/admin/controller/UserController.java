d94705ff690d373e24441f2a0837d418dcc122d2
zhixiong.huang <zhixiong.huang@hulu.com>
2023-07-05 21:46:15 +0800

    update

--- user-demo-admin/src/main/java/org/example/user/admin/controller/UserController.java
+++ user-demo-admin/src/main/java/org/example/user/admin/controller/UserController.java
@@ -66,7 +66,7 @@ public class UserController {
                     message = "The users are deleted successfully"
             ),
             @ApiResponse(code = Status.HTTP_BAD_REQUEST, message = "If any fields are missing from the request."),
-            @ApiResponse(code = Status.HTTP_INTERNAL_ERROR, message = "Internal server error. Ping a IDM dev."),
+            @ApiResponse(code = Status.HTTP_INTERNAL_ERROR, message = "Internal server error."),
             @ApiResponse(code = Status.HTTP_UNAVAILABLE, message = "Service is down, Caller should wait and retry.")
     })
     @DeleteMapping(value = "/{ids}")
